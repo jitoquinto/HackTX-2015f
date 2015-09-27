ef5965e5fedcd2f0565c899138209dbdd38133bf
Kaveet Laxmidas <kaveetlaxmidas@gmail.com>
2015-09-26 20:05:37 -0500

    Implement functions and preliminary SQL connection

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function buildConn($configs) {
+  $sqlServer = $configs['dbHost'];
+  $sqlUser = $configs['dbUser'];
+  $sqlPass = $configs['dbPass'];
+  $sqlName = $configs['dbName'];
+
+  try {
+    $conn = new PDO("mysql:host=$sqlServer;dbname=$sqlName", $sqlUser, $sqlPass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "SQL Connection established.";
+  }
+  catch(PDOException $e) {
+    $genError = $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
+    die();
+  }
+return $conn;
+}
+
+?>
